54d716e12f67ccfc4207806ef0b2a533fd4b9e53
BangTepllo <117557846+BangTepllo@users.noreply.github.com>
2023-10-04 07:06:58 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,28 +1,27 @@
 const fs = require('fs')
 
-global.creator = 'Admin SF'// yourname
-global.apikey = ["AdminSf", "sfmd", "SatanFusion"]
-global.MONGO_DB_URI = "mongodb+srv://sfvpnteam752:1tMKf3H2oPcz0ChN@cluster0.l5btbqh.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "sfvpnteam752@gmail.com" //email
-global.email_password = "ytylpwyklevmafpu" //application password email
+global.creator = 'Ikhsan'// yourname
+global.MONGO_DB_URI = "mongodb+srv://ShanBot:shanbot@shanbot.74phqya.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "shanbot" //isi apa aja bebas
+global.your_email = "shanbot20@gmail.com" //email
+global.email_password = "lotlgtbgbgltpxke" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
 	noapikey:{
 		status: 403,
-        message: 'Input parameter apikey',
+        message: 'apikeynya mana deck!',
         creator: `${creator}`,
         result: "error"
     },
     error: {
         status: 503,
-        message: 'Service Unavaible, Sedang dalam perbaikan',
+        message: 'Service tidak tersedia, Sedang dalam perbaikan',
         creator: `${creator}`
     },
     apikey: {
     	status: 403,
-    	message: 'Forbiden, Invalid apikey',
+    	message: 'Terlarang, Invalid apikey',
     	creator: `${creator}`
     },
     noturl: {
